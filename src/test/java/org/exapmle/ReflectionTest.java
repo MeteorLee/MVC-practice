d96ffd57a1771c535c59352bb27cd26331e9f191
MeteorLee <meteorproject529@gmail.com>
2023-07-13 23:13:47 +0900

    study: heap 영역에 로드되어 있는 class타입의 객체를 가져오는 방법
    3가지 방법으로 실습

--- src/test/java/org/exapmle/ReflectionTest.java
+++ src/test/java/org/exapmle/ReflectionTest.java
@@ -1,5 +1,6 @@
 package org.exapmle;
 
+import org.assertj.core.api.Assertions;
 import org.example.annotation.Controller;
 import org.example.annotation.Service;
 import org.example.model.User;
@@ -14,6 +15,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @Controller 애노테이션이 설정돼 있는 모든 클래스를 찾아서 출력한다
@@ -39,6 +42,31 @@ public class ReflectionTest {
         logger.debug("User all declared constructors: [{}]", Arrays.stream(clazz.getDeclaredConstructors()).collect(Collectors.toList()));
         logger.debug("User all declared Methods: [{}]", Arrays.stream(clazz.getDeclaredMethods()).collect(Collectors.toList()));
         
+    }
+
+
+    // heap 영역에 로드되어 있는 class타입의 객체를 가져오는 방법
+    @Test
+    void load() throws ClassNotFoundException {
+        // 1번째 방법
+        Class<User> clazz1 = User.class;
+
+        // 2번째 방법
+        User user = new User("serverWizard", "이호승");
+        Class<? extends User> clazz2 = user.getClass();
+
+        // 3번째 방법
+        Class<?> clazz3 = Class.forName("org.example.model.User");
+
+        // 3가지 모두 같은 객체를 얻을 수 있다
+        logger.debug("clazz1: [{}]", clazz1);
+        logger.debug("clazz2: [{}]", clazz2);
+        logger.debug("clazz3: [{}]", clazz3);
+
+        assertThat(clazz1 == clazz2).isTrue();
+        assertThat(clazz1 == clazz3).isTrue();
+        assertThat(clazz2 == clazz3).isTrue();
+
     }
 
     private static Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
